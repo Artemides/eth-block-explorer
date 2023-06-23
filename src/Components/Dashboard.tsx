3763a4b5bea5f8f9509b959f7fb25c1551904196
Artemides <edmpulasky@gmail.com>
2023-06-22 22:31:54 -0500

    enhance block reward

--- src/Components/Dashboard.tsx
+++ src/Components/Dashboard.tsx
@@ -2,42 +2,46 @@
 
 import React, { useCallback, useContext, useEffect, useState } from "react";
 import { AlchemyContext } from "../Context/AlchemyProvider";
-import { BigNumber, Block, Utils } from "alchemy-sdk";
+import { BigNumber, Block, BlockWithTransactions, Utils } from "alchemy-sdk";
 
 export const Dashboard =  () => {
   const { alchemy } = useContext(AlchemyContext);
   const [blockNumber, setBlockNumber] = useState(0);
 
-  const getTransactionGasUsed=useCallback(async(transactionHash:string)=>{
-    console.log("getTransactionGasUsed")
-    const transactionReceipt=await alchemy?.core.getTransactionReceipt(transactionHash);
-    const gasUsed=transactionReceipt?.gasUsed ?? 0;
-    return gasUsed;
-  },[alchemy?.core])
-
   const getBlockReward=useCallback(async (block:Block)=>{
     console.log("getBlockReward")
     const blockBaseFeePerGas=block.baseFeePerGas ?? BigNumber.from(0);
     const blockGasUsed=block.gasUsed;
-    const blockTransactions=block.transactions;
+    const BlockWithTransactions =await alchemy?.core.getBlockWithTransactions(block.number);
+    const transactions=BlockWithTransactions?.transactions;
+    const transactionsReceipts=await alchemy?.core.getTransactionReceipts({blockHash:block.hash});
+    // const blockTransactions=block.transactions;
     const minerTips=[];
-    console.log({blockTransactions:blockTransactions.length})
-    for(const transaction of blockTransactions ){
-        const tx=await alchemy?.core.getTransaction(transaction);
-        const gasPrice=tx?.gasPrice ?? 0;
-        const gasUsed=await getTransactionGasUsed(transaction);
-        const transactionFee=Utils.formatEther(BigNumber.from(gasUsed).mul(gasPrice));
+
+    if(transactionsReceipts && transactionsReceipts.receipts&&transactions){
+      for(const transaction of transactions ){
+        console.log({transaction:transaction.hash})
+        const gasPrice=transaction.gasPrice ?? BigNumber.from(0);
+        const transactionReceipt=transactionsReceipts.receipts.find(tx=>tx.transactionHash===transaction.hash);
+        if(!transactionReceipt) continue ;
+
+        const gasUsed=transactionReceipt.gasUsed;
+        const transactionFee=BigNumber.from(gasUsed).mul(gasPrice);
+        console.log({transactionFee})
+        
         minerTips.push(transactionFee)
     } 
+    }
+
+   
     const TransactionsFee=minerTips.reduce((totalFee,fee)=>totalFee.add(fee),BigNumber.from(0))
-    const burnedFee=Utils.formatEther(blockBaseFeePerGas.mul(blockGasUsed));
+    const burnedFee=blockBaseFeePerGas.mul(blockGasUsed);
     
     const baseBlockReward=BigNumber.from(2);
-
     const blockReward= baseBlockReward.add(TransactionsFee).sub(burnedFee);
     console.log({blockReward})
-    return blockReward.toString();
-  },[alchemy?.core, getTransactionGasUsed])
+    return blockReward;
+  },[alchemy?.core])
 
   useEffect(() => {
     if (!alchemy) return;
@@ -46,8 +50,8 @@ export const Dashboard =  () => {
       console.log({block})
       const blockData=await alchemy.core.getBlock(block);
       console.log({blockData})
-      const blockReward=await getBlockReward(blockData);
-      console.log({blockReward})
+      const blockReward=(await getBlockReward(blockData));
+      console.log({blockReward:Utils.formatEther(blockReward).toString()})
       setBlockNumber(block);
   
     };
