c6d9a8bc6312d3e41360bc33ceea01e2010c0ba4
Artemides <edmpulasky@gmail.com>
2023-06-23 17:06:54 -0500

    improve and move block reward calculation to alchemy context

--- src/Components/Dashboard.tsx
+++ src/Components/Dashboard.tsx
@@ -2,57 +2,22 @@
 
 import React, { useCallback, useContext, useEffect, useState } from "react";
 import { AlchemyContext } from "../Context/AlchemyProvider";
-import { BigNumber, Block, BlockWithTransactions, Utils } from "alchemy-sdk";
+import { BigNumber, Block, BlockWithTransactions, TransactionReceipt, TransactionResponse, Utils } from "alchemy-sdk";
 
+type BlockStats={
+  number:number,
+  reward:string
+}
 export const Dashboard =  () => {
-  const { alchemy } = useContext(AlchemyContext);
-  const [blockNumber, setBlockNumber] = useState(0);
-
-  const getBlockReward=useCallback(async (block:Block)=>{
-    console.log("getBlockReward")
-    const blockBaseFeePerGas=block.baseFeePerGas ?? BigNumber.from(0);
-    const blockGasUsed=block.gasUsed;
-    const BlockWithTransactions =await alchemy?.core.getBlockWithTransactions(block.number);
-    const transactions=BlockWithTransactions?.transactions;
-    const transactionsReceipts=await alchemy?.core.getTransactionReceipts({blockHash:block.hash});
-    // const blockTransactions=block.transactions;
-    const minerTips=[];
-
-    if(transactionsReceipts && transactionsReceipts.receipts&&transactions){
-      for(const transaction of transactions ){
-        console.log({transaction:transaction.hash})
-        const gasPrice=transaction.gasPrice ?? BigNumber.from(0);
-        const transactionReceipt=transactionsReceipts.receipts.find(tx=>tx.transactionHash===transaction.hash);
-        if(!transactionReceipt) continue ;
-
-        const gasUsed=transactionReceipt.gasUsed;
-        const transactionFee=BigNumber.from(gasUsed).mul(gasPrice);
-        console.log({transactionFee})
-        
-        minerTips.push(transactionFee)
-    } 
-    }
-
-   
-    const TransactionsFee=minerTips.reduce((totalFee,fee)=>totalFee.add(fee),BigNumber.from(0))
-    const burnedFee=blockBaseFeePerGas.mul(blockGasUsed);
-    
-    const baseBlockReward=BigNumber.from(2);
-    const blockReward= baseBlockReward.add(TransactionsFee).sub(burnedFee);
-    console.log({blockReward})
-    return blockReward;
-  },[alchemy?.core])
-
+  const { alchemy,getBlockReward } = useContext(AlchemyContext) as AlchemyContext;
+  const [latestBlock, setLatestBlock] = useState<BlockStats>({number:0,reward:'0'});
   useEffect(() => {
-    if (!alchemy) return;
     const getLatestBlock = async () => {
-      const block = await alchemy.core.getBlockNumber();
-      console.log({block})
-      const blockData=await alchemy.core.getBlock(block);
-      console.log({blockData})
-      const blockReward=(await getBlockReward(blockData));
-      console.log({blockReward:Utils.formatEther(blockReward).toString()})
-      setBlockNumber(block);
+      const blockNumber = await alchemy.core.getBlockNumber();
+      const block=await alchemy.core.getBlock(blockNumber);
+      const blockReward=await getBlockReward(block);
+      const blockRewardEther=Utils.formatEther(blockReward).toString();
+      setLatestBlock({number:blockNumber,reward:blockRewardEther});
   
     };
     getLatestBlock();
@@ -60,5 +25,8 @@ export const Dashboard =  () => {
 
 
 
-  return <div>Latest Block: {blockNumber}</div>;
+  return <div className="flex gap-4 p-4 ring-2 ring-sky-500 rounded-md shadow-lg bg-sky-800/60">
+      <div>Latest Block: {latestBlock.number}</div>
+      <div>Block Reward: {latestBlock.reward}</div>
+  </div>;
 };
--- src/Context/AlchemyProvider.tsx
+++ src/Context/AlchemyProvider.tsx
@@ -1,21 +1,63 @@
 "use client";
-import { Alchemy, AlchemySettings, Network } from "alchemy-sdk";
-import { PropsWithChildren, createContext, useState } from "react";
+import { Alchemy, AlchemySettings, BigNumber, Block, Network, TransactionReceipt } from "alchemy-sdk";
+import { PropsWithChildren, createContext, useCallback, useState } from "react";
 
-type AlchemyContext = {
-  alchemy?: Alchemy;
+export  type AlchemyContext = {
+  alchemy: Alchemy;
+  getBlockReward:(block:Block)=>Promise<BigNumber>;
 };
 
-export const AlchemyContext = createContext<AlchemyContext>({});
+export const AlchemyContext = createContext<AlchemyContext|null>(null);
 const settings: AlchemySettings = {
   apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY,
   network: Network.ETH_MAINNET,
 };
 export const AlchemyProvider = ({ children }: PropsWithChildren) => {
   const alchemy = new Alchemy(settings);
+  const findTransactionsReceipt= (receipts:TransactionReceipt[],transactionHash:string)=>{
+    const transactionReceipt=receipts.find(tx=>tx.transactionHash===transactionHash);
+    return transactionReceipt;
+  }
+  const getTransactionsFee = useCallback(async (blockHash:string):Promise<BigNumber>=>{
+    let transactionsFee=BigNumber.from(0);
+    const transactionsReceipts=await alchemy.core.getTransactionReceipts({blockHash});
+    const BlockWithTransactions =await alchemy.core.getBlockWithTransactions(blockHash);
+    if(!transactionsReceipts ||!BlockWithTransactions) return transactionsFee;
+
+
+    const transactions=BlockWithTransactions.transactions;
+    const receipts=transactionsReceipts.receipts;
+    if(!receipts) return transactionsFee;
+
+    for(const transaction of transactions ){
+      const gasPrice=transaction.gasPrice ?? BigNumber.from(0);
+      const transactionReceipt= findTransactionsReceipt(receipts,transaction.hash);
+      if(!transactionReceipt) continue ;
+
+      const gasUsed=transactionReceipt.gasUsed;
+      const transactionFee=BigNumber.from(gasUsed).mul(gasPrice);
+      transactionsFee=transactionsFee.add(transactionFee);
+    }
+    console.log({transactionsFee})
+    return transactionsFee;
+  },[alchemy.core])
+
+  const getBlockReward=useCallback(async (block:Block)=>{
+    console.log("getBlockReward")
+    const blockBaseFeePerGas=block.baseFeePerGas ?? BigNumber.from(0);
+    const blockGasUsed=block.gasUsed;
+    const transactionsFee=await  getTransactionsFee(block.hash);
+    console.log({transactionsFee})
+
+    const burntFee=blockBaseFeePerGas.mul(blockGasUsed);
+    const baseBlockReward=BigNumber.from(2);
+    const blockReward= baseBlockReward.add(transactionsFee).sub(burntFee);
+    console.log({blockReward})
+    return blockReward;
+  },[getTransactionsFee])
 
   return (
-    <AlchemyContext.Provider value={{ alchemy }}>
+    <AlchemyContext.Provider value={{ alchemy,getBlockReward }}>
       {children}
     </AlchemyContext.Provider>
   );
