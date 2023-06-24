c6dd3b38d3133c701db344c4a474f16147409cbb
Artemides <edmpulasky@gmail.com>
2023-06-23 19:39:28 -0500

    add navbar

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { MdInsights } from "react-icons/md";
+export const Navbar = () => {
+  return (
+    <nav className='sticky top-0 flex items-center gap-4 w-full py-4 px-16 bg-black/25 '>
+        <MdInsights size={24}/>
+        <span className=' font-bold text-2xl z-2'>EtherInsight</span>
+    </nav>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { AlchemyProvider } from "../Context/AlchemyProvider";
 import { Dashboard } from "../Components/Dashboard";
+import { Navbar } from "@/Components/Navbar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between bg-gradient-radial from-indigo-950 from-10%  to-black ">
       <AlchemyProvider>
+        <Navbar/>
         <Dashboard />
       </AlchemyProvider>
     </main>
