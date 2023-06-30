83a79f65f87bbd7d595bdb3aaab246445d9ded35
Artemides <edmpulasky@gmail.com>
2023-06-29 22:38:41 -0500

    use icons

--- src/app/transactions/[blockHash]/page.tsx
+++ src/app/transactions/[blockHash]/page.tsx
@@ -5,6 +5,8 @@ import { AlchemyContext } from "@/Context/AlchemyProvider";
 import { Block, BlockWithTransactions, TransactionResponse } from "alchemy-sdk";
 import moment from "moment";
 import React, { useContext, useEffect, useMemo, useState } from "react";
+import { BiTime, BiGasPump } from "react-icons/bi";
+import { BsBox } from "react-icons/bs";
 
 type BlockWithTransactionsProps = {
     blockHash: string;
@@ -40,17 +42,23 @@ const BlockWithTransactions = ({
             .fromNow();
         return date;
     }, [block]);
-    console.log({ blockHash });
+
     return (
-        <article className="m-8 flex flex-col gap-8">
+        <article className="m-8 flex flex-col  gap-8">
             <h1 className="text-2xl font-bold">Transactions </h1>
-            <div className="flex gap-4 px-4 py-8 ring-1 ring-sky-500/20 rounded-md divide-x-2 divide-sky-500/30">
-                <span className="text-3xl font-bold">
-                    Block: #{block?.number}
-                </span>
-                <span className="text-3xl font-bold pl-4">
-                    Age: {blockDate}
-                </span>
+            <div className="flex justify-around gap-4 px-4 py-8 ring-1 ring-sky-500/20 rounded-md ">
+                <div className="text-3xl font-bold">
+                    <BsBox size={32} className="inline-block align-top mr-2" />
+                    <span>Block: #{block?.number}</span>
+                </div>
+                <div className="text-3xl font-bold">
+                    <BiTime size={32} className="inline-block align-top mr-2" />
+                    <span>Age: {blockDate}</span>
+                </div>
+                <div className="text-3xl font-bold">
+                    <BiGasPump size={32} className="inline-block align-top" />
+                    <span> Gas: {block?.gasUsed.toString()}</span>
+                </div>
             </div>
             {block && (
                 <div className="flex flex-col gap-2 8 ring-1 ring-sky-500/20 rounded-md">
