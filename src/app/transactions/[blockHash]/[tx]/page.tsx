ff54c4b4cc978243d137cadecbbc068c27b7e8e9
Artemides <edmpulasky@gmail.com>
2023-07-03 14:44:02 -0500

    render transaction from to value

--- src/app/transactions/[blockHash]/[tx]/page.tsx
+++ src/app/transactions/[blockHash]/[tx]/page.tsx
@@ -1,22 +1,44 @@
 "use client";
 
 import { Chip } from "@/Components/Chip";
+import { Divider } from "@/Components/Divider";
 import TransactionStatus from "@/Components/TransactionStatus";
 import { AlchemyContext } from "@/Context/AlchemyProvider";
-import { TransactionReceipt } from "alchemy-sdk";
+import {
+    BigNumber,
+    Block,
+    BlockTag,
+    TransactionReceipt,
+    TransactionResponse,
+    Utils,
+} from "alchemy-sdk";
+import moment from "moment";
 import React, { useContext, useEffect, useState } from "react";
 
 const Transaction = ({ params }: { params: { tx: string } }) => {
     const { alchemy } = useContext(AlchemyContext) as AlchemyContext;
     const { tx } = params;
-    const [transaction, setTransaction] = useState<TransactionReceipt | null>();
+    const [transaction, setTransaction] = useState<TransactionResponse | null>(
+        null
+    );
+    const [transactionReceipt, setTransactionReceipt] =
+        useState<TransactionReceipt | null>(null);
 
+    const [block, setblock] = useState<Block | null>(null);
     useEffect(() => {
         if (!tx) return;
         const retrieveTransaction = async () => {
-            const response = await alchemy.core.getTransactionReceipt(tx);
+            const response = await alchemy.core.getTransaction(tx);
+            const txReceipt = await alchemy.core.getTransactionReceipt(tx);
             console.log({ response });
+            setTransactionReceipt(txReceipt);
             setTransaction(response);
+            if (!response) return;
+
+            const thisBlock = await alchemy.core.getBlock(
+                response.blockHash as BlockTag
+            );
+            setblock(thisBlock);
         };
         retrieveTransaction();
     }, [alchemy.core, tx]);
@@ -28,22 +50,47 @@ const Transaction = ({ params }: { params: { tx: string } }) => {
                 className=" p-4 ring-1 ring-sky-500/20 rounded-md"
             >
                 <div className="grid grid-cols-[max-content,1fr] gap-x-8 gap-y-4">
-                    <Chip
-                        title="Transaction Hash"
-                        value={transaction?.transactionHash}
-                    />
+                    <Chip title="Transaction Hash" value={transaction?.hash} />
 
                     <Chip
                         title="Status"
                         value={
                             <TransactionStatus
                                 status={
-                                    transaction && transaction.status === 1
+                                    transaction &&
+                                    transactionReceipt?.status === 1
                                         ? "confirmed"
                                         : "failed"
                                 }
                             />
                         }
+                    />
+                    <Chip
+                        title="Block"
+                        value={transaction?.blockNumber}
+                        description={`${
+                            transaction?.confirmations ?? 0
+                        } Block Confirmations`}
+                    />
+                    {block && (
+                        <Chip
+                            title="Timestamp"
+                            value={`${moment(block.timestamp * 1000)
+                                .startOf("hour")
+                                .fromNow()} (${moment(
+                                block.timestamp * 1000
+                            ).format("MMMM Do YYYY, h:mm:ss a")})`}
+                        />
+                    )}
+                    <Divider className="col-span-2" />
+
+                    <Chip title="From" value={transaction?.from ?? "0x"} />
+                    <Chip title="To" value={transaction?.to ?? "0x"} />
+                    <Chip
+                        title="Value"
+                        value={Utils.formatEther(
+                            BigNumber.from(transaction?.value ?? 0)
+                        )}
                     />
                 </div>
             </article>
